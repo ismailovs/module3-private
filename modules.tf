c313a63722c714cfac9408b2856b16c637e94f7f
Shukhrat Ismailov <shuhx7@gmail.com>
2023-11-11 08:47:12 -0500

    updated

--- modules.tf
+++ modules.tf
@@ -1,6 +1,6 @@
-module "module-security-groups" {
-  source  = "app.terraform.io/s_tc_1_remote/module-security-groups/aws"
-  version = "1.0.1"
+module "security_groups" {
+  source  = "app.terraform.io/s_tc_1/security_groups/aws"
+  version = "1.0.0"
   vpc_id = aws_vpc.main.id
   security_groups = var.security_groups
   # insert required variables here!
